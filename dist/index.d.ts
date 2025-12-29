cc31a2cffe90c0a167e3c7e790c1f596da51a858
NyanHa <154053992+DevNyanHa@users.noreply.github.com>
2025-12-30 05:36:29 +0900

    Delete dist/index.d.ts

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { I18N } from "./core";
-export type { I18nOptions } from "./core";
-export { Locale, normalize } from "./locales";
-export type { LocaleValue } from "./locales";
-export type { Messages } from "./message";
